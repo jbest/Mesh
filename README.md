e75dbe48de38ce806796547ba0e7eba8df7b785b
Jason Best <jbest@brit.org>
2013-11-12 00:18:06 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Mesh - router_test
+=========
+
+This script will just create a connection to an XBee configured as a router and will send characters to the network Coordinator. With a XBee coordinator properly configured, you will see the characters in a tarminal session connected to the Coordinator.
+
+
+##Parts
+1 - Arduino Uno
+2 - XBee radio - https://www.adafruit.com/products/968
+2 - XBee Adaptor Kit - https://www.adafruit.com/products/126
+
+
+##Configuration of XBee
+
+The Adafruit tutorial is out of date or incomplete. See parts 1 and 2 of tunnelsup on YouTube:
+http://www.youtube.com/watch?v=odekkumB3WQ
+http://www.youtube.com/watch?v=mPx3TjzvE9U
+
+##TODO
+
+Add wiring diagram based on http://cs.smith.edu/dftwiki/index.php/Tutorial:_Arduino_and_XBee_Communication
---
+++ router_test/router_test.ino
@@ -0,0 +1,56 @@
+// From http://cs.smith.edu/dftwiki/index.php/Tutorial:_Arduino_and_XBee_Communication
+
+/*
+  Xbee1
+  D. Thiebaut
+  
+  Makes Arduino send 1 character via XBee wireless to another XBee connected
+  to a computer via a USB cable. 
+
+  The circuit: 
+  * RX is digital pin 2 (connect to TX of XBee)
+  * TX is digital pin 3 (connect to RX of XBee)
+ 
+  Based on a sketch created back in the mists of time by Tom Igoe
+  itself based on Mikal Hart's example
+ 
+*/
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial xbee(2, 3); // RX, TX
+char c = 'A';
+int  pingPong = 1;
+
+void setup()  {
+   //Serial.begin(57600);
+   Serial.begin(9600);
+   Serial.println( "Arduino started sending bytes via XBee" );
+
+   // set the data rate for the SoftwareSerial port
+   //xbee.begin( 19200 );
+   xbee.begin( 9600 );
+}
+
+void loop()  {
+  // send character via XBee to other XBee connected to Mac
+  // via USB cable
+  xbee.print( c );
+  
+  //--- display the character just sent on console ---
+  Serial.println( c );
+  
+  //--- get the next letter in the alphabet, and reset to ---
+  //--- 'A' once we have reached 'Z'. 
+  c = c + 1;
+  if ( c>'Z' ) 
+       c = 'A';
+  
+  //--- switch LED on Arduino board every character sent---
+  if ( pingPong == 0 )
+    digitalWrite(13, LOW);
+  else
+    digitalWrite(13, HIGH);
+  pingPong = 1 - pingPong;
+  delay( 1000 );
+}
